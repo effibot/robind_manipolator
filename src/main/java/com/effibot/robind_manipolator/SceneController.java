93163c8059ba88e25811142360ce1bde99c93894
Effibot <andrea.efficace1@gmail.com>
2022-08-04 20:46:43 +0200

    feat: scene completed. SceneController edited.

--- src/main/java/com/effibot/robind_manipolator/SceneController.java
+++ src/main/java/com/effibot/robind_manipolator/SceneController.java
@@ -1,6 +1,11 @@
 package com.effibot.robind_manipolator;
 import com.effibot.robind_manipolator.Processing.*;
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,75 +14,84 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.SegmentedButton;
+import org.controlsfx.control.textfield.CustomTextField;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-
 public class SceneController implements Initializable, Observer {
     @FXML
+    public SegmentedButton segButtonBar;
+    @FXML
+    private Button cancel;
+    @FXML
+    private Tab controlTab;
+    @FXML
+    private ComboBox method;
+    @FXML
+    private Button startBtn;
+    @FXML
+    private Label objName;
+    @FXML
+    private Button bckCtrlBtn;
+    @FXML
+    private Button bckStpBtn;
+    @FXML
+    private CustomTextField rollField;
+    @FXML
+    private CustomTextField pitchField;
+    @FXML
+    private CustomTextField yawField;
+    @FXML
     private Button back;
-
     @FXML
     private ImageView basicMap;
-
     @FXML
     private AnchorPane bottomAnchor;
-
     @FXML
     private ToggleButton coneBtn;
-
     @FXML
     private Button cont;
-
     @FXML
     private ToggleButton cubeBtn;
-
     @FXML
     private AnchorPane leftAnchor;
-
     @FXML
     private SplitPane mainView;
-
-
     @FXML
     private ImageView map;
-
-    @FXML
-    private Label obsNumber;
-
     @FXML
     private AnchorPane paneOne;
-
     @FXML
     private AnchorPane paneTwo;
-
     @FXML
     private Label pathLabel;
-
     @FXML
     private AnchorPane rightAnchor;
-
     @FXML
     private ToggleButton sphereBtn;
-
     @FXML
     private AnchorPane topAnchor;
-
     @FXML
     private SplitPane topSplit;
     @FXML
     private TabPane tabPane;
     @FXML
     private Tab setupTab, infoTab;
+    private final ToggleGroup group = new ToggleGroup();
+
+    private final ObservableList<String> methods = FXCollections.observableArrayList("Parabolic", "Quintic", "Cubic");
+    private float roll, pitch, yaw;
+    private ArrayList<Integer> sequence;
 
     @FXML
     public void onContinueButtonClick() {
 //        ArrayList<Obstacle> dummy = new ArrayList<>();
 //        dummy.add(new Obstacle(sketch,2*(40-10),2*(40-10),50,120,100,0));
 //        ((P2DMap)sketch).setObstacleList(dummy);
-        if(obsList != null) {
+        if (obsList != null) {
             // load images
             Image basicMapImage = new Image(String.valueOf(getClass().getResource("img/mappaPre.jpeg")),
                     256d, 256d, true, true);
@@ -88,8 +102,8 @@ public class SceneController implements Initializable, Observer {
             map.setImage(postProcMapImage);
             setupTab.setClosable(true);
             setupTab.setDisable(true);
-            tabPane.getSelectionModel().select(infoTab);
-            infoTab.setDisable(false);
+            tabPane.getSelectionModel().select(controlTab);
+            controlTab.setDisable(false);
             // close 2D map
             sketch.removeObserver(this);
             sketch.noLoop();
@@ -104,10 +118,11 @@ public class SceneController implements Initializable, Observer {
             //TODO: implements popup to specify at leas one obstacle
         }
     }
+
     @FXML
     public void onCancelButtonClick() {
-        obsList.remove(obsList.size()-1);
-        ((P2DMap)sketch).setObstacleList((ArrayList<Obstacle>) obsList);
+        obsList.remove(obsList.size() - 1);
+        ((P2DMap) sketch).setObstacleList((ArrayList<Obstacle>) obsList);
     }
 
     @FXML
@@ -115,18 +130,45 @@ public class SceneController implements Initializable, Observer {
         //TODO: implementa azione basata sull'oggetto chiamante
         // in base al bottone che chiama il metodo, carica l'oggetto
 
-        String id = ((Node)actionEvent.getSource()).getId();
+        String id = ((Node) actionEvent.getSource()).getId();
         switch (id) {
-            case "cube":
-                System.out.println(id);
+            case "sphere":
+                if (sequence.isEmpty() || !sequence.contains(0)) {
+                    sequence.add(0);
+                } else {
+                    sequence.removeIf(t -> t == 0);
+                }
                 break;
             case "cone":
-                System.out.println(id);
+                if (sequence.isEmpty() || !sequence.contains(1)) {
+                    sequence.add(1);
+                } else {
+                    sequence.removeIf(t -> t == 1);
+                }
                 break;
-            case "sphere":
-                System.out.println(id);
+            case "cube":
+                if (sequence.isEmpty() || !sequence.contains(2)) {
+                    sequence.add(2);
+                } else {
+                    sequence.removeIf(t -> t == 2);
+                }
                 break;
         }
+        System.out.println(sequence);
+    }
+
+    @FXML
+    public void onStartAction(ActionEvent actionEvent) {
+        String interp = ((String) method.getValue()).toLowerCase();
+        roll = Integer.parseInt(rollField.getText());
+        pitch = Integer.parseInt(pitchField.getText());
+        yaw = Integer.parseInt(yawField.getText());
+        boolean condition = sequence.isEmpty() || method.getSelectionModel().isEmpty() ||
+                rollField.getText().isEmpty() || pitchField.getText().isEmpty() || rollField.getText().isEmpty();
+        if (condition) {
+
+        }
+
 
     }
 
@@ -146,21 +188,59 @@ public class SceneController implements Initializable, Observer {
         topDivider.positionProperty().addListener((observable, oldValue, newValue) -> topDivider.setPosition(topDividerPosition));
 
         // Setup delle label d'info
-        obsNumber.setText("");
+        objName.setText("");
         pathLabel.setText("");
         infoTab.setClosable(true);
         infoTab.setDisable(true);
+        // setup label control
+        controlTab.setClosable(true);
+        controlTab.setDisable(true);
+        method.getItems().addAll("Parabolic", "Quintic", "Cubic");
+        method.setVisibleRowCount(3);
+        method.setPromptText("Metodo");
+        // Setup RPY default value
+        rollField.setText(String.valueOf(0));
+        addTextListener(rollField);
+        pitchField.setText(String.valueOf(0));
+        addTextListener(pitchField);
+        yawField.setText(String.valueOf(0));
+        addTextListener(yawField);
+        // init sequence
+        sequence = new ArrayList<>();
+
+        startBtn.setDisable(false);
     }
+
+    private void addTextListener(CustomTextField txtField) {
+        txtField.textProperty().addListener((observable, oldValue, newValue) -> {
+                    if (!newValue.matches("\\d{0,7}([\\.]\\d{0,4})?")) {
+                        rollField.setText(oldValue);
+                    }
+//                    if (newValue.equals(String.valueOf(0)) || oldValue.equals(String.valueOf(0)))
+//                        rollField.setText(oldValue + "." + newValue);
+                    //TODO doppio zero va trimmato e mezzo a singolo 0
+                    else if (newValue.equals("."))
+                        rollField.setText("0" + newValue);
+                }
+        );
+    }
+
     // Processing 2D setup
     public ProcessingBase sketch;
     Application app;
+
     public void setJavafxApp(Application jfxApp) {
         app = jfxApp;
     }
-    public void setSketch(ProcessingBase sketch) { this.sketch = sketch; }
+
+    public void setSketch(ProcessingBase sketch) {
+        this.sketch = sketch;
+    }
+
     private List<Obstacle> obsList;
+
     @Override
     public void update(Object object) {
-        this.obsList = ((P2DMap)object).getObstacleList();
+        this.obsList = ((P2DMap) object).getObstacleList();
     }
 }
